2029a46b6afb9aaf7b6a0ef770cea960cce083e6
Gilles Oldano <gilles.oldano@gmail.com>
2024-05-28 19:00:21 +0200

    Added nvim configurations

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+  "catppuccin/nvim", 
+  name = "catppuccin", 
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      default_integration = true,
+    })
+    vim.cmd.colorscheme "catppuccin-frappe"
+  end
+}
+
---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,25 @@
+return {
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- lua
+				null_ls.builtins.formatting.stylua,
+
+				-- js
+				require("none-ls.diagnostics.eslint_d"),
+				null_ls.builtins.formatting.prettier,
+
+				-- python
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.black,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
