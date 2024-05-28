f19ccbf86696ecdbd4f7a87d348a67236ba0e400
Luca Pasini <luca.pasini@supsi.ch>
2024-05-28 22:58:37 +0200

    feat: open neotree when opening "nvim <directory>"

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -8,5 +8,17 @@ return {
   },
   config = function()
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+  end,
+  init = function()
+    if vim.fn.argc(-1) == 1 then
+      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == "directory" then
+        require("neo-tree").setup({
+          filesystem = {
+            hijack_netrw_behavior = "open_current",
+          },
+        })
+      end
+    end
   end
 }
